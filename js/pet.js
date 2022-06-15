827569dbacb64f1ace1bcbd05c579efc2a8c8753
joaopedroalc <joaopedro.ufma@gmail.com>
2022-06-15 10:27:23 -0300

    ok

--- js/pet.js
+++ js/pet.js
@@ -1,5 +1,18 @@
 const form = document.querySelector(".cadastraPet")
 
+let apiUrl = 'http://localhost:8089/PetLost';
+
+function pegarBackend() {
+  const pegaApi = fetch(apiUrl)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log(data);
+    })
+  return pegaApi
+}
+pegarBackend();
+
+var uploaded_image;
 function cadastrarPet() {
   form.style.display = 'block';
   form.style.visibility = 'visible';
@@ -9,13 +22,75 @@ function cadastrarPet() {
   image_input.addEventListener("change", function () {
     const reader = new FileReader();
     reader.addEventListener("load", () => {
-      const uploaded_image = reader.result;
+      uploaded_image = reader.result;
       document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
     });
     reader.readAsDataURL(this.files[0]);
   });
 
 }
+
+const nomePet = document.querySelector('input[name="Texto"]').value;
+const localizacao = document.querySelector('input[name="Local"]').value;
+const imagem = document.querySelector("#display-image").style.backgroundImage;
+
+function petCard() {
+  const infosForm = document.querySelector(".form");
+  infosForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const infos = {
+      nomePet: nomePet,
+      localizacao: localizacao,
+      imagem: imagem
+    }
+    //JOGAR DADOS PARA O BANCO
+    console.log(infos);
+
+    let card = ` 
+    <div class="card">
+    <img src="${uploaded_image}" alt="dog" />
+    <h2 class="nomePet">${nomePet}</h2>
+    <button
+      type="button"
+      class="btn-modal"
+      onclick="abrirModal()"
+    >
+      Ver informações
+    </button>
+    <div class="${nomePet} hidden"></div>
+  </div>`
+    const cards = document.querySelector('.cards')
+    cards.innerHTML += card
+
+
+  });
+}
+function abrirModal() {
+  return `
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="${nomePet}">
+        ${nomePet}
+      </h5>
+      <button class="close-modal" onclick="fecharModal()">X</button>
+    </div>
+    <div class="modal-body">
+      <p>${localizacao}</p>
+    </div>
+    <div class="modal-footer">
+      <button
+        type="button"
+        class="close-modal"
+      >
+        Fechar
+      </button>
+    </div>
+  </div>
+`
+}
+petCard()
+abrirModal()
 
 function salvarDadosPet() {
   // SALVAR DADOS NO BANCO AO CLICAR NO BOTAO ANUNCIAR
