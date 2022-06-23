449beacbe16179a38b2e65c95e7972571aea9eb3
joaopedroalc <joaopedro.ufma@gmail.com>
2022-06-23 14:19:01 -0300

    evento de deletar no botao lixeira remove o card conforme id do doc

--- js/salvarDadosFirestore.js
+++ js/salvarDadosFirestore.js
@@ -40,12 +40,16 @@ infosForm.addEventListener("submit", function (e) {
 
   db.collection("PetLost").add(infos)
     .then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
+      // console.log("Document written with ID: ", docRef.id);
+      alert("Pet Cadastrado com sucesso!");
+      document.location.reload(true);
     })
     .catch((error) => {
       console.error("Error adding document: ", error);
     });
 });
+
+let arrayDeIds = [];
 
 function mostraDados() {
   db.collection("PetLost").get().then((querySnapshot) => {
@@ -56,13 +60,31 @@ function mostraDados() {
         <img src="${doc.data().imagem}" alt="dog" class="img-card" />
         <h2 class="${doc.data().nomePet}">Nome do Pet: ${doc.data().nomePet}</h2>
         <p>Descrição do Pet: ${doc.data().description}</p>
+        <img src="https://png.pngtree.com/png-vector/20190905/ourlarge/pngtree-trash-icon-with-pastel-color-png-image_1723138.jpg" class='lixeira ${doc.id}' />
     </div>`
       const cards = document.querySelector('.cards')
       cards.innerHTML += card
+
+      arrayDeIds.push(doc.id)
+      //REMOVE O CARD CLICADO CONFORME O ID
+      arrayDeIds.map((arrayDeId) => {
+        const lixo = document.querySelector(`.${arrayDeId}`);
+
+        lixo.addEventListener("click", () => {
+          db.collection("PetLost").doc(arrayDeId).delete().then(() => {
+            console.log("Document successfully deleted!");
+            document.location.reload(true);
+          }).catch((error) => {
+            console.error("Error removing document: ", error);
+          });
+        });
+
+      })
     });
   });
 }
 mostraDados()
+
 
 
 
